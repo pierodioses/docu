e10cb2d4b88e3d8fd0a55ac9d85c2b408ad0d521
pierodioses <pieroaromm2004@gmail.com>
2024-01-31 16:16:04 -0500

    cambin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My docusaurus',
+  title: 'My docusaurus1',
   tagline: 'Dinosaurs pieero dioses',
   favicon: 'https://www.staffcreativa.pe/blog/wp-content/uploads/apple-logo1.jpg',
 
@@ -16,7 +16,7 @@ const config = {
   url: 'https://pierodioses.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docu/',
+  baseUrl: '/caso3/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
